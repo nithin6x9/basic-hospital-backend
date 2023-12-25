9dc26566810bb92f4f7c73d4d9a8bb7278c3c1e1
nithin-nk <nithinkrishnalal303@gmail.com>
2023-12-25 15:52:34 +0530

    sum server

--- middleware.js
+++ middleware.js
@@ -1,4 +1,9 @@
-function usermiddlware(req,res,next){
+const express = require("express");
+const app = express()
+
+const port = 3000
+
+function usermiddleware(req,res,next){
     if(username!="nithin" || pasword!="pass"){
         res.status(400).json({
             msg:"User not found",
@@ -7,4 +12,25 @@ function usermiddlware(req,res,next){
         next();
     }
 };
-function kidneymiddleware)()
+function kidneymiddleware(req,res,next){
+    if(kidnyeid !=1 && kidneyid != 2){
+        res.status.json({
+            msg:"Kidney count invalid",
+        })
+    }else{
+        next();
+    }
+};
+
+app.use(calcaulaterequests) 
+app.get("/health-checkup",usermiddleware,kidneymiddleware,(req,res)=>{
+    app.send("your heart is healthy");
+});
+
+app.get("/health-checkup",(req,res)=>{
+    res.send("your heart is healthy");
+});
+
+app.get("/health-checkup",usermiddleware,(req,res)=>{
+    res.send("your heart is healthy");
+})
