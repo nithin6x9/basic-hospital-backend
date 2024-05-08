b60ab6e7521a7c24fc789d6e71e54eeaf0b524ac
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-05-08 20:18:19 +0530

    Cloud

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -9,10 +9,32 @@
  * `Env` object can be regenerated with `npm run cf-typegen`.
  *
  * Learn more at https://developers.cloudflare.com/workers/
- */
+
+export default {
+    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+        return Response.json({
+            message:"Fuck u bitch!"
+        });
+	},
+};
+
+*/
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body);
+		console.log(request.headers);
+        console.log(request.method);
+        
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
 };
